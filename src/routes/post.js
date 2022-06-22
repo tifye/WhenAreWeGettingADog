0b553e496975cdcc07d7cacbebccc30f22ed578a
tifufu <dematasjoshua@hotmail.com>
2022-06-22 21:45:59 +0200

    final touches

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const { createDog, removeDog } = require('../data-access');
 
-const router = express.Router();
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, '../public/images'),
+  filename: (req, file, cb) => {
+    cb(null, `${file.originalname}`);
+  }
+});
 
-router.post('/', async (req, res) => {
+const router = express.Router();
+const upload = multer({ storage: storage });
+
+router.post('/', upload.single('image'), async (req, res) => {
   const dog = req.body;
+  dog.imageLink = req.file.filename;
   await createDog(dog);
   res.redirect('#dogs');
 });
